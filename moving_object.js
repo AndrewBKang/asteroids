6adc23fe37daf6c493165d84c7270bf3d907e7d7
Angelo and Scott <Angelo+Scott@appacademy.io>
2013-07-23 10:41:33 -0400

    Add moving object class

---
+++ moving_object.js
@@ -0,0 +1,33 @@
+var MovingObjects = (function () {
+
+  function MovingObject(centerX,centerY,radius) {
+    this.centerX = centerX;
+    this.centerY = cetnerY;
+    this.radius = radius
+  };
+
+  MovingObject.MAX_RADIUS = 25;
+  MovingObject.randomMovingObject = function (maxX,maxY) {
+    return new MovingObject(
+      maxX * Math.random(),
+      maxY * Math.random(),
+      MovingObject.MAX_RADIUS * Math.random()
+    );
+  };
+
+  MovingObject.prototype.update = function (deltaX,deltaY) {
+    this.centerX += deltaX;
+    this.centerY += deltaY;
+  };
+
+  MovingObject.prototype.offScreen = function (screenX, screenY) {
+    return (
+      (this.centerX > this.radius + screenX) ||
+      (this.centerY > this.radius + screenY) ||
+      (this.centerX + this.radius) < 0 ||
+      (this.centerY + this.radius) < 0
+    )
+
+  }
+
+})();
